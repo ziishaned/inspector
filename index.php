52fb8640b8d19a49a31872464223ba5fddeea5cb
Zeeshan Ahmed <ziishaned@gmail.com>
2016-06-23 16:10:56 +0500

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-
-use Zeeshan\Inspector\Inspector;
-
-$inspector = new Inspector('e0a5c0bfd84a458e');
-$person = $inspector->getProfile("kamranahmed.se@gmail.com");
